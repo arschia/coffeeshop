312645263b5c007dcfb0895b74c8dd9830bd22f5
1introspection <arshiyaeskandriyan@gmail.com>
2025-02-07 19:48:49 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+      boxShadow: {
+        normal: "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        // ============== Dana ============== //
+        Dana: ["Dana"],
+        "dana-Medium": ["Dana Medium"],
+        "dana-DemiBold": ["Dana DemiBold"],
+
+        // ============== Morabba ============== //
+        "morabba-Medium": ["Morabba Medium"],
+        "morabba-Light": ["Morabba Light"],
+        "morabba-Bold": ["Morabba Bold"],
+      },
+      letterSpacing: {
+        tightest: "-0.065em",
+
+      },
+      spacing: {
+        "30": "7.5rem",
+        "25": "6.25rem",
+        "50": "12.5rem",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          lg: "0.625rem",
+        },
+      },
+      backgroundImage: {
+        "home-mobile": "url(../images/headerBgMobile.webp)",
+        "home-desktop": "url(../images/headerBgDesktop.webp)",
+      }
+    },
+    screens: {
+      xxs:"400px",
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
+};
+
